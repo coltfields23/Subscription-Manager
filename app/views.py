f32715c5b7044e408c978eb6a9526cd10a919db0
coltfields23 <ccfields2003@gmail.com>
2022-11-30 15:31:14 -0600

    First

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseRedirect
+from .forms import NewUserForm, NewTaskForm
+from app.models import Task
+
+# Auth views
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("CreateTask")
+    form = NewUserForm()
+    return render(
+        request=request, template_name="signup.html", context={"register_form": form}
+    )
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("CreateTask")
+    form = AuthenticationForm()
+    return render(
+        request=request, template_name="login.html", context={"login_form": form}
+    )
+
+def logout_request(request):
+    logout(request)
+    return redirect("CreateTask")
+
+
